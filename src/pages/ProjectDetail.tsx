db942728f00d4265711a2799c1b2867e5b89a249
langeconstrucaokadu-spec <langeconstrucaokadu@gmail.com>
2026-06-17 10:56:20 -0300

    Mudei novamente o botão de exportação

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -267,7 +267,7 @@ const ProjectDetail = () => {
                     toast.error((e as Error).message ?? "Falha ao exportar.");
                   }
                 }}>
-                  <Download className="size-4" /> Exportar Xoxota
+                  <Download className="size-4" /> Exportar Cronograma
                 </Button>
               </div>
             )}
